03d65185449d6a788112f865055fa0eb05b390a1
Masanori Hiroki <hiroki.masanori06@gmail.com>
2019-09-15 15:50:10 +0900

    Add config.fish

---
+++ config.fish
@@ -0,0 +1,13 @@
+
+# install fisherman
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+# sync bashrc to fish setting
+# make -C ~/dotfiles/Bash2FishAliasesSync sync; and source ~/.config/fish/b2f_aliases.fish
+
+# ls setting
+export LSCOLORS=cxfxcxdxbxegedabagacad
+
