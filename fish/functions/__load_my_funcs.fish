b3db9db9e74442b99ac7c986b9d9b718885e6835
Masanori Hiroki <hiroki.masanori06@gmail.com>
2019-09-16 18:47:50 +0900

    Refact __load_my_funcs

--- fish/functions/__load_my_funcs.fish
+++ fish/functions/__load_my_funcs.fish
@@ -1,8 +1,14 @@
+# reffer it
+# https://qiita.com/positrium/items/8dd695123d6023c53ff5
 function __load_my_funcs
   # TODO: better management
   #  - dir recursive
   #  - when increased functions, use fisher
-  for file_name in (ls ./my)
-   source ./my/$file_name
+  set -l DIR_NAME my
+
+  for file_name in (ls ./$DIR_NAME)
+   source ./$DIR_NAME/$file_name
   end
+
+  echo 'success to load my functions!'
 end
