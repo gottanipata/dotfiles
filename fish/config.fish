1f405c08d4e2de64aad383c496876233b9a32de1
Masanori Hiroki <hiroki.masanori06@gmail.com>
2019-11-17 17:27:54 +0900

    Move install fisherman to function

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,13 @@
-# install fisherman
-if not functions -q fisher
+# reference
+# https://fishshell.com/docs/current/tutorial.html
+function install_fisherman
+  if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
+  end
 end
+install_fisherman
 
 function set_data_science_tools
   if not test -e ~/.config/data-science-at-the-command-line/tools/
