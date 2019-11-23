383467b8ceb2168ec4cb102ed22e3f5d222130af
Masanori Hiroki <hiroki.masanori06@gmail.com>
2019-11-23 10:34:51 +0900

    Fix to load my functions

--- fish/functions/__load_my_funcs.fish
+++ fish/functions/__load_my_funcs.fish
@@ -4,10 +4,10 @@ function __load_my_funcs
   # TODO: better management
   #  - dir recursive
   #  - when increased functions, use fisher
-  set -l DIR_NAME my
+  set -l DIR_PATH ~/dotfiles/fish/functions/my
 
-  for file_name in (ls ./$DIR_NAME)
-   source ./$DIR_NAME/$file_name
+  for file_name in (ls $DIR_PATH)
+   source $DIR_PATH/$file_name
   end
 
   echo 'success to load my functions!'
